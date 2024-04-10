2ecab02989414535afea4f7301e53c992b4d0c65
Andres <andres.antonio2000@hotmail.com>
2024-04-10 12:54:17 -0400

    Primera interfaz web

--- 1-API-REST/src/js/sendForm.js
+++ 1-API-REST/src/js/sendForm.js
@@ -1,1 +1,24 @@
-document.
+document.querySelector('form').addEventListener('submit', async (event) => {
+    event.preventDefault();
+    const formData = new FormData(event.target);
+    const data = Object.fromEntries(formData);
+    console.log(data);
+
+    try {
+        const response = await fetch('http://localhost:3000/crear_usuario', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(data),
+        });
+        if (response.ok) {
+            const json = await response.json();
+            console.log(json);
+        } else {
+            console.error('Error');
+        }
+    } catch (error) {
+        console.error('Error');
+    }
+});
