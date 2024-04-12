e8ea608d1723bdb58be1be277c8af79fdfb48b65
Andres <andres.antonio2000@hotmail.com>
2024-04-12 18:50:33 -0400

    Update user model and HTML files

--- 1-API-REST/models/user.js
+++ 1-API-REST/models/user.js
@@ -1,14 +1,47 @@
+/*
+Vamos a simular una base de datos de usuarios con SQLite3
+Para ello vamos a crear una clase Usuarios que tendrá los siguientes métodos y propiedades:
+Propiedades:
+- id: number
+- nombre: string
+- email: string
+- edad: number
+- fecha de nacimiento: string
+- género: string
+- número de teléfono: string
+- dirección: string
+- última fecha de inicio de sesión: string
+- estado de la cuenta: string
+- rol del usuario: string
+- avatar: string
+*/
+
+
+
 const db = require("../conexion");
 
 class Usuarios {
-    constructor(id, nombre, email, edad){
+    constructor(id, nombreUsuario, email, password, edad, fechaNacimiento, genero, telefono, direccion, ultimaFechaInicioSesion, estadoCuenta, rolUsuario, avatar){
         this.id = id;
-        this.nombre = nombre;
+        this.username = nombreUsuario;
         this.email = email;
+        this.password = password;
         this.edad = edad;
+        this.fechaNacimiento = fechaNacimiento;
+        this.genero = genero;
+        this.telefono = telefono;
+        this.direccion = direccion;
+        this.ultimaFechaInicioSesion = ultimaFechaInicioSesion;
+        this.estadoCuenta = estadoCuenta;
+        this.rolUsuario = rolUsuario;
+        this.avatar = avatar;
     }
-    async save(){
-        await db.run(`INSERT INTO usuarios (nombre, edad, email) VALUES ("${this.nombre}", ${this.edad}, "${this.email}")`)
+    save(){
+        new Promise ()aadb.run(`INSERT INTO usuarios (nombre, email, password, edad, fecha_nacimiento, genero, telefono, direccion, ultima_fecha_inicio_sesion, estado_cuenta, rol_usuario, avatar) VALUES ("${this.username}", "${this.email}", "${this.password}", ${this.edad}, "${this.fechaNacimiento}", "${this.genero}", "${this.telefono}", "${this.direccion}", "${this.ultimaFechaInicioSesion}", "${this.estadoCuenta}", "${this.rolUsuario}", "${this.avatar}")`, (err)=>{
+            if(err){
+                console.error(`Error al crear el usuario: ${err.message}`)
+            };
+        }
     }
     
     static mostrarUsuarios(){
--- 1-API-REST/test.js
+++ 1-API-REST/test.js
@@ -12,3 +12,4 @@ function pruebaESLint() {
   
   pruebaESLint();
   
+
